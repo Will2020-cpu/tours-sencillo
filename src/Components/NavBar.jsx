6c1ddc1ae35bc6fdc000a450e773b685f2690723
Madara <wcamacho221@gmail.com>
2021-02-08 13:45:28 -0500

    Tours sencillo

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+
+const NavBar = () => {
+    return (
+        <Fragment>
+            <div className="w-full bg-blue-400">
+                <div className="w-9/12 m-auto flex items-center justify-between">
+                    <div><a href="">Logo</a></div>
+                    <nav>
+                       
+                    </nav>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default NavBar
